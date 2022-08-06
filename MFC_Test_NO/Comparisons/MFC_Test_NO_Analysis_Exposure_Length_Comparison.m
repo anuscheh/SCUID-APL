40427af756f9634e21927bc06b61da53180a6aee
Cyano-Hibiki <jerrychen0828@gmail.com>
2022-08-05 17:38:18 -0700

    Exposure Length Comparisons

--- MFC_Test_NO/Comparisons/MFC_Test_NO_Analysis_Exposure_Length_Comparison.m
+++ MFC_Test_NO/Comparisons/MFC_Test_NO_Analysis_Exposure_Length_Comparison.m
@@ -10,13 +10,13 @@ ignore_rh_chk = true;
 rsp_smooth = true;
 % - Enable/disable manual step width (in seconds)
 step_man_range = true;
-step_man_width = 180;
+% step_man_width = 180;
 % - Hide/Show Figures:
 minimize_figures = false;
 if minimize_figures == true
     figure_state = "minimized";
 else
-    figure_state = "normal";
+    figure_state = "maximized";
 end
 % - Time window finding method: 
 % ----- true  = auto define from flow file info
@@ -39,11 +39,52 @@ else
     t_f = 4.8 * 60 * 60;
 end
 
+% ==========> Comparison Settings <==========
+movmean_k = 30;
+exposure_length = [60; 120; 180; 240];
+designated_pad = 12;
+
 %% LOADING DATA
 
 % I put the .mat file in the same directory as this script. Make sure you
 % specify the directory when you use this script.
-load("CNT_Results_NO.mat")
+data_loc = cd;
+data_loc = fullfile(data_loc, '..');
+load(fullfile(data_loc, "CNT_Results_NO.mat"))
+
+
+%% Moving mean range comparison figures
+exp_len_comp_rvc = figure('Name', ...
+    'Exposure Length Comparison - Response vs. Concentration', ...
+    'NumberTitle', 'off');
+exp_len_comp_rvc.WindowState = figure_state;
+tiledlayout(1,1)
+ax4 = nexttile;
+hold(ax4, "on")
+legend()
+title({'Response vs. Concentration', 'Using Different Lengths of Time', ...
+    'for Mean Concentration'});
+subtitle({"Chip " + num2str(CNT_Results_NO(entry).chip) + " Pad " + ...
+    designated_pad + ", " + CNT_Results_NO(entry).addinfo, 'Moving Mean with 30 points'});
+xlabel('NO Concentration')
+ylabel('Response');
+ax4.FontSize = 16;
+
+exp_len_comp_rvt = figure('Name', ...
+    'Exposure Length Comparison - Response vs. Time', ...
+    'NumberTitle', 'off');
+exp_len_comp_rvt.WindowState = figure_state;
+tiledlayout(1,1)
+ax5 = nexttile;
+hold(ax5, "on")
+legend()
+title({'Response vs. Time', 'Using Different Lengths of Time', ...
+    'for Mean Concentration'});
+subtitle({"Chip " + num2str(CNT_Results_NO(entry).chip) + " Pad " + ...
+    designated_pad + ", " + CNT_Results_NO(entry).addinfo, 'Moving Mean with 30 points'});
+xlabel('Time')
+ylabel('Response');
+ax5.FontSize = 16;
 
 %% DATA PROCESSING
 % Define start of collection to be zero second.
@@ -73,110 +114,51 @@ noppm_wndw = CNT_Results_NO(entry).noppm(run_range);
 NO_free_indices = find(~noppm_wndw);
 
 % Process the noppm data to remove pre-spikes.
-noppm_spike = figure('Name', 'NO Spike Removal', 'NumberTitle', 'off');
-noppm_spike.WindowState = figure_state;
-plot(time_wndw, noppm_wndw, 'r-'); hold on;
+% noppm_spike = figure('Name', 'NO Spike Removal', 'NumberTitle', 'off');
+% noppm_spike.WindowState = figure_state;
+% plot(time_wndw, noppm_wndw, 'r-'); hold on;
 
 noppm_wndw_rmspk = hampel(noppm_wndw, 15);
-plot(time_wndw, noppm_wndw_rmspk, 'b-', LineWidth=2); 
-title('NO Concentration over time')
-legend('Original Data', 'Pre-Spikes Removed')
-xlabel('Time (s)')
-ylabel('Concentration')
-hold off
+% plot(time_wndw, noppm_wndw_rmspk, 'b-', LineWidth=2); 
+% title('NO Concentration over time')
+% legend('Original Data', 'Pre-Spikes Removed')
+% xlabel('Time (s)')
+% ylabel('Concentration')
+% hold off
 
 % Auto detect start and end of exposure
 stp_i = detect_start(noppm_wndw_rmspk);
 disp('beginings')
 disp(stp_i)
 stp_f = zeros(size(stp_i));
-if step_man_range == false
-    stp_f = detect_end(noppm_wndw_rmspk);
-else
-    for stp = 1:length(stp_i)
-        [~, end_ind] = min(abs((time_wndw(stp_i(stp)) + step_man_width)-time_wndw));
-        stp_f(stp) = end_ind;
-    end
-    disp('endings')
-    disp(stp_f)
-end
-disp('intervals')
-disp(time_wndw(stp_f) - time_wndw(stp_i))
+% if step_man_range == false
+%     stp_f = detect_end(noppm_wndw_rmspk);
+% else
+%     for stp = 1:length(stp_i)
+%         [~, end_ind] = min(abs((time_wndw(stp_i(stp)) + step_man_width)-time_wndw));
+%         stp_f(stp) = end_ind;
+%     end
+%     disp('endings')
+%     disp(stp_f)
+% end
+% disp('intervals')
+% disp(time_wndw(stp_f) - time_wndw(stp_i))
 %%
-% average concentration within each exposure
-noppm_stp_avg = zeros(size(stp_i));
-for step = 1:length(stp_i)
-    noppm_stp_avg(step) = mean(noppm_wndw_rmspk(stp_i(step):stp_f(step)));
-end
+for pad = designated_pad
 
-% Plotting response vs concentration (raw response)
-rsp_vs_noppm = figure('Name', 'Response vs. Concentration', ...
-    'NumberTitle', 'off');
-rsp_vs_noppm.WindowState = figure_state;
-tiledlayout(1,1)
-ax1 = nexttile;
-hold(ax1, "on")
-legend()
-title('Response at Different Added NO Concentration');
-subtitle(strcat('Chip ', num2str(CNT_Results_NO(entry).chip), ', Pads 7-12, ', CNT_Results_NO(entry).addinfo));
-xlabel('NO Concentration');
-ylabel('Response');
-
-% Plotting response over time (baseline corrected)
-rsp_blc = figure('Name', 'Response - Baseline Corrected', ...
-    'NumberTitle', 'off');
-rsp_blc.WindowState = figure_state;
-tiledlayout(1,1)
-ax2 = nexttile;
-hold(ax2, "on")
-legend()
-title('Response Over Time - Baseline Corrected');
-subtitle("Chip " + num2str(CNT_Results_NO(entry).chip) + " Pads 7-12, " + ...
-    CNT_Results_NO(entry).addinfo);
-xlabel('Time (s)')
-ylabel('Response');
-
-% Plotting response over time (raw)
-rsp_raw = figure('Name', 'Response - Raw Data', ...
-    'NumberTitle', 'off');
-rsp_raw.WindowState = figure_state;
-tiledlayout(1,1)
-ax3 = nexttile;
-hold(ax3, "on")
-legend()
-title('Response Over Time - Raw Data');
-subtitle("Chip " + num2str(CNT_Results_NO(entry).chip) + " Pads 7-12, " + ...
-    CNT_Results_NO(entry).addinfo);
-xlabel('Time (s)')
-ylabel('Response');
-%%
-for pad = 7:12
-    % pick out data for specific pad
+    % pick out response data for specific pad
     r_pad = CNT_Results_NO(entry).r(:,pad);
     r_pad_wndw = r_pad(run_range);
 
     % normalize data using r0 = response at end of pre-purge
     r0 = r_pad_wndw(stp_i(1)-10);
     r_pad_wndw_norm = (r_pad_wndw)./r0 * 100;
-    plot(ax3, time_wndw, movmean(r_pad_wndw_norm, 15), ...
-        DisplayName=['Pad ' num2str(pad)])
-
-    rsp_raw = figure('Name', "Pad "+pad+" Response - Raw", 'NumberTitle', 'off');
-    rsp_raw.WindowState = figure_state;
-    plot(time_wndw, movmean(r_pad_wndw_norm,15)); hold on;
    
     %　BASELINE CORRECTION
     % Curve fitting on data when there isn't NO exposure
     X = time_wndw(NO_free_indices);
     Y = r_pad_wndw_norm(NO_free_indices);
     [r_pad_wndw_blfit, gof] = fit(X, Y, 'exp1');
-    
-    % Visual check of fit
-    plot(r_pad_wndw_blfit, time_wndw, r_pad_wndw_norm);
-    title('Response Before Baseline Correction')
-    subtitle("chip " + num2str(CNT_Results_NO(entry).chip) + " - pad " + ...
-        num2str(pad) + " - date: " + CNT_Results_NO(entry).addinfo)
-    hold off;
     disp(gof)
 
     % Subtracting baseline from original response data
@@ -190,30 +172,60 @@ for pad = 7:12
 
     disp(length(time_wndw_rmol))
     disp(length(r_pad_wndw_blred_rmol))
-    
-    rsp_blc = figure('Name', "Pad "+pad+" Response - Baseline Corrected", ...
-        'NumberTitle', 'off');
-    rsp_blc.WindowState = figure_state;
-    if rsp_smooth == true
-        r_pad_wndw_blred_rmol = movmean(r_pad_wndw_blred_rmol, 15);
-    end
-    plot(time_wndw_rmol, r_pad_wndw_blred_rmol)
-    title('Response After Baseline Correction')
-    subtitle("chip " + num2str(CNT_Results_NO(entry).chip) + " - pad " + ...
-        num2str(pad) + " - date: " + CNT_Results_NO(entry).addinfo)
-    
-    % Add plot to response over time graph (all pads in one)
-    plot(ax2, time_wndw_rmol, r_pad_wndw_blred_rmol, ...
-        DisplayName=['Pad ' num2str(pad)])
-    
 
-    % Add plot to response vs concentration graph (all pads in one)
-    max_rsp = zeros(size(stp_i));
-    for step = 1:length(stp_i)
-        max_rsp(step) = max(r_pad_wndw_blred_rmol(stp_i(step):stp_f(step)));   
+    % Response Data Smoothening
+    r_pad_wndw_blred_rmol_smth = r_pad_wndw_blred_rmol;
+    if rsp_smooth == true
+        r_pad_wndw_blred_rmol_smth = movmean(r_pad_wndw_blred_rmol_smth, movmean_k);
     end
-    plot(ax1, noppm_stp_avg, max_rsp, '-.', DisplayName=['Pad ' num2str(pad)])
+    
+    % Plot Response over time curve (same for all)
+    plot(ax5, time_wndw_rmol, r_pad_wndw_blred_rmol_smth, 'k', ...
+        DisplayName = ['Response (movmean k=', num2str(movmean_k), ')'], LineWidth= 1.5)
+
+    % Plot each exposure length.
+    for j = 1:length(exposure_length)
+        disp("current exposure length is")
+        disp(exposure_length(j,1))
+
+        % Define End of Step
+        for stp = 1:length(stp_i)
+            [~, end_ind] = min(abs((time_wndw(stp_i(stp)) + exposure_length(j,1))-time_wndw));
+            stp_f(stp) = end_ind;
+        end
+
+        % average concentration within each exposure
+        noppm_stp_avg = zeros(size(stp_i));
+        for step = 1:length(stp_i)
+            noppm_stp_avg(step) = mean(noppm_wndw_rmspk(stp_i(step):stp_f(step)));
+        end
+        
+        % Responses at end of defined length
+        end_rsp = r_pad_wndw_blred_rmol_smth(stp_f);
+        disp('========> Response value picked are:')
+        disp(end_rsp)
+
+        plot(ax4, noppm_stp_avg, end_rsp, '.-', MarkerSize=18, ...
+            DisplayName = strcat("length=", num2str(exposure_length(j,1)), ...
+            ' seconds'), LineWidth= 1.5)
+
+        
+        
+        plot(ax5, time_wndw_rmol(stp_f), end_rsp, '*', MarkerSize=24, ...
+            DisplayName = strcat('R sample point for length=', ...
+            num2str(exposure_length(j,1)), ' seconds'), LineWidth= 1.5)
+    end
+%     color_reorder = colororder(ax5);
+%     color_reorder = kron(color_reorder,ones(2,1));
+%     disp(color_reorder)
+%     colororder(ax5, color_reorder)
 end
+
+%% Saving Figure(s)
+figfilename1 = ['/MFC_Test_NO_Analysis_Exposure_Length_Comparison_Results/chip_' num2str(CNT_Results_NO(entry).chip) '_pad_'  num2str(designated_pad)  '_rvc.png'];
+saveas(exp_len_comp_rvc,[pwd figfilename1]);
+figfilename2 = ['/MFC_Test_NO_Analysis_Exposure_Length_Comparison_Results/chip_' num2str(CNT_Results_NO(entry).chip) '_pad_'  num2str(designated_pad)  '_rvt.png'];
+saveas(exp_len_comp_rvt,[pwd figfilename2]);
 
 %% CUSTOM FUNCTIONS
 % Detect Start of Exposure
