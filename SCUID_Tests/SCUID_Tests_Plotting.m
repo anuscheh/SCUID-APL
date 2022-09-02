ca719c1b3b5c87a78b5c9160d7caff931ae36375
Anuscheh Nawaz <anuscheh@uw.edu>
2022-09-02 15:30:14 -0700

    data from 2022-9-2, and feedback for plotting
    in plotting script, I cannot get the "fontsize" to work, so I commented it out for now. Also, the plots look odd, the 20 minutes I entered for when the gas was injected turned into negative time..I think you want us to enter clock time?
    Data from 2022-9-2 added.

--- SCUID_Tests/SCUID_Tests_Plotting.m
+++ SCUID_Tests/SCUID_Tests_Plotting.m
@@ -49,7 +49,7 @@ plot(ts,SCUID_Test_Results(tgt_entry,1).RH,'DisplayName',...
 xlabel(ax_rh,'Time [s]');
 ylabel(ax_rh,'Relative Humidity');
 legend(ax_rh,'boxoff');
-fontsize(ax_rh,20,'points');
+%fontsize(ax_rh,20,'points');
 hold(ax_rh,'off');
 
 % Temperature Plot
@@ -65,7 +65,7 @@ plot(ax_temp,ts,SCUID_Test_Results(tgt_entry,1).Temp1,'DisplayName',...
 xlabel(ax_temp,'Time [s]');
 ylabel(ax_temp,['Temperature [' char(176) 'C]']);
 legend(ax_temp,'boxoff');
-fontsize(ax_temp,20,'points');
+%fontsize(ax_temp,20,'points');
 hold(ax_temp,'off')
 
 % Pressure Plot
@@ -81,7 +81,7 @@ plot(ax_p,ts,SCUID_Test_Results(tgt_entry,1).P1,'DisplayName',...
 xlabel(ax_p,'Time [s]');
 ylabel(ax_p,'Pressure [mBar]');
 legend(ax_p,'boxoff');
-fontsize(ax_p,20,'points');
+%fontsize(ax_p,20,'points');
 hold(ax_p,'off')
 
 % Sensors 1-6 Plot
@@ -97,7 +97,7 @@ end
 xlabel(ax_sg1,'Time [s]');
 ylabel(ax_sg1,"Response")
 legend(ax_sg1,'boxoff');
-fontsize(ax_sg1,20,'points');
+%fontsize(ax_sg1,20,'points');
 hold(ax_sg1,'off');
 
 % Sensors 7-12 Plot
@@ -113,7 +113,7 @@ end
 xlabel(ax_sg2,'Time [s]');
 ylabel(ax_sg2,"Response")
 legend(ax_sg2,'boxoff');
-fontsize(ax_sg2,20,'points');
+%fontsize(ax_sg2,20,'points');
 hold(ax_sg2,'off');
 
 % Plotting injection x lines
@@ -124,8 +124,8 @@ for i = 1:length(SCUID_Test_Results(tgt_entry,1).InjectionTime)
     xline_pos = time_inj_ue - SCUID_Test_Results(tgt_entry,1).TimeUE(1);
     for ax = [ax_rh ax_temp ax_p ax_sg1 ax_sg2]
         display_name = strcat(gas_type," Injection ",num2str(i));
-        xline(ax,xline_pos,'-',display_name,...
-            FontSize=20,LineWidth=2,HandleVisibility='off')
+        xline(ax,xline_pos,'-',display_name)
+           % FontSize=20,LineWidth=2,HandleVisibility='off')
     end
 end
 
