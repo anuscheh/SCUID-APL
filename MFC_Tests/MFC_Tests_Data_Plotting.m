8675430c0f73d95985adf1939466d94bdd5a4434
Anuscheh Nawaz <anuscheh@uw.edu>
2022-09-30 12:11:48 -0700

    9/29 and 30 testing

--- MFC_Tests/MFC_Tests_Data_Plotting.m
+++ MFC_Tests/MFC_Tests_Data_Plotting.m
@@ -9,26 +9,26 @@ clear; close all; clc;
 % -> Test Date 
 target_date = datetime("2022-09-29","Format","yyyy-MM-dd");
 % -> Target Board & Chip
-target_board = 0;
-target_chip = 19;
+target_board = 1;
+target_chip = 18;
 % -- Pads info
 num_pads = 12;
-target_pads = 7:12;
+target_pads = 1:6;
 % -> Gas info
 gas_type = "NO";
 gas_conc = 12.9;
-gas_humidity = "Dry";
+gas_humidity = "RH";
 mfc_name = "MFC1";
 % -> Time window info
-num_runs = 1;
-num_steps = 3;      % number of steps per run
-run_length = 3060;  % can be calculated from flow files; total run length in seconds, plus 1/2 of the purge in between runs.
+num_runs = 2;
+num_steps = 4;      % number of steps per run
+run_length = 4800;  % can be calculated from flow files; total run length in seconds, plus 1/2 of the purge in between runs.
 step_length = 120;  % seconds for NO exposure
 prepurge = 1800;     % seconds
 min_conc = 0.1;     % Concentration of the lowest step, in [ppm].
 sample_rate = 2;    % How many samples per second?
 
-target_entry = 52; % <<<<<<<<<<<< CHANGE THIS
+target_entry = 56; % <<<<<<<<<<<< CHANGE THIS, this is the row in the struct file we want to evaluate
 
 %% Data Processing Options (Only Change When Needed!)
 % Automatically detect rising edge of concentration data.
