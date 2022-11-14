72fdd09ce844139cce497ac19a1419896538f27c
Anuscheh Nawaz <anuscheh@uw.edu>
2022-11-14 09:38:47 -0800

    plots

--- MFC_Tests/MFC_Tests_Data_Plotting.m
+++ MFC_Tests/MFC_Tests_Data_Plotting.m
@@ -28,7 +28,7 @@ clear; close all; clc;
 
 %% Basic Test Information <= MUST CHANGE EVERYTIME!
 % -> Test Date 
-target_date = datetime("2022-11-08","Format","yyyy-MM-dd");
+target_date = datetime("2022-11-11","Format","yyyy-MM-dd");
 % -> Target Board & Chip
 target_board = 0;
 target_chip = 22;
@@ -36,20 +36,20 @@ target_chip = 22;
 num_pads = 12;
 target_pads = 7:12;
 % -> Gas info
-gas_type = "N2O";
+gas_type = "NO";
 gas_conc = 12.9;
 gas_humidity = "RH";
 mfc_name = "MFC1";
 % -> Time window info
-num_runs = 1;
+num_runs = 2;
 num_steps = 3;      % number of steps per run
-run_length = 2880;  % can be calculated from flow files; total run length in seconds, plus 1/2 of the purge in between runs.
+run_length = 7000;  % can be calculated from flow files; total run length in seconds, plus 1/2 of the purge in between runs.
 step_length = 120;  % seconds for NO exposure
-prepurge = 1200;     % seconds
-min_conc = 0.1;     % Concentration of the lowest step, in [ppm].
+prepurge = 600;     % seconds
+min_conc = 0.5;     % Concentration of the lowest step, in [ppm].
 sample_rate = 2;    % How many samples per second?
 temp_range = [20,30];
-target_entry = 108; % <<<<<<<<<<<< CHANGE THIS, this is the row in the struct file we want to evaluate
+target_entry = 111; % <<<<<<<<<<<< CHANGE THIS, this is the row in the struct file we want to evaluate
 
 %% Data Processing Options (Only Change When Needed!)
 % Automatically detect rising edge of concentration data.
